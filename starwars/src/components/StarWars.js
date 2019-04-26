2c7c9522da7c66eea43299e0538806806faaad13
Kayla <6lackmacaroon@gmail.com>
2019-04-26 12:59:07 -0600

    almost MVP needs styling

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -7,8 +7,10 @@ function Chars (props) {
            <div className='card'>
                   <h2>{props.thing.name}</h2>
                 <ul>
+                      <li>{props.thing.birth_year}</li>
+                      <li>{props.thing.height}</li>
                       <li>{props.thing.weight}</li>
-                      <li>{props.thing.weight}</li>
+                      <li>{props.thing.mass}</li>
                 </ul>
            </div>
      );
